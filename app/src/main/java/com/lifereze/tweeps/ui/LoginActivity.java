6cd4d438de81b8b51ab4db8f749b5fd245dae3b7
lifereze <liferezearnold@gmail.com>
2020-08-09 22:38:57 +0300

    add new user activity logic

--- app/src/main/java/com/lifereze/tweeps/ui/LoginActivity.java
+++ app/src/main/java/com/lifereze/tweeps/ui/LoginActivity.java
@@ -1,4 +1,117 @@
 package com.lifereze.tweeps.ui;
 
-public class LoginActivity {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.kenneth.spoileralert.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.loginButton) Button mLoginButton;
+    @Bind(R.id.newUserTextView) TextView mNewUserTextView;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private ProgressDialog mLoginProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user!=null){
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        mLoginButton.setOnClickListener(this);
+        mNewUserTextView.setOnClickListener(this);
+        showProgressDialog();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mAuthStateListener!=null){
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v==mLoginButton){
+            existingUserLogin();
+        }
+        if (v==mNewUserTextView){
+            Intent intent = new Intent(LoginActivity.this,NewUserActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void existingUserLogin(){
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")){
+            mEmailEditText.setError("Please enter a valid email address");
+            return;
+        }
+        if (password.equals("")){
+            mPasswordEditText.setError("Password field is empty. Please fill it in");
+        }
+
+        mLoginProgressDialog.show();
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            mLoginProgressDialog.dismiss();
+                            Toast.makeText(LoginActivity.this, "Signed in!", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Sign in failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void showProgressDialog(){
+        mLoginProgressDialog = new ProgressDialog(this);
+        mLoginProgressDialog.setMessage("Logging you in...");
+        mLoginProgressDialog.setCancelable(false);
+    }
 }
--- app/src/main/java/com/lifereze/tweeps/ui/NewUserActivity.java
+++ app/src/main/java/com/lifereze/tweeps/ui/NewUserActivity.java
@@ -1,4 +1,136 @@
 package com.lifereze.tweeps.ui;
 
-public class NewUserActivity {
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.kenneth.spoileralert.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewUserActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.newUserSubmitButton) Button mNewUserSubmitButton;
+    @Bind(R.id.userLoginTextView) TextView mUserLoginTextView;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+        ButterKnife.bind(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mNewUserSubmitButton.setOnClickListener(this);
+        mUserLoginTextView.setOnClickListener(this);
+        createAuthStateListener();
+        createAuthProgressDialog();
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v==mNewUserSubmitButton){
+            createNewUser();
+        }
+        if (v==mUserLoginTextView){
+            Intent intent = new Intent(NewUserActivity.this,LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+    }
+
+    private void createAuthProgressDialog(){
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setMessage("Please wait while we authenticate you ...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    private void createAuthStateListener(){
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!=null){
+                    Intent intent = new Intent(NewUserActivity.this,MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    private void createNewUser(){
+        final String email = mEmailEditText.getText().toString().trim();
+        final String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+        boolean validEmail = isValidEmail(email);
+        boolean validPassword = isValidPassword(password,confirmPassword);
+        if (!validEmail||!validPassword)return;
+        mAuthProgressDialog.show();
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    mAuthProgressDialog.dismiss();
+                    Toast.makeText(NewUserActivity.this, "Authentication Successful", Toast.LENGTH_SHORT).show();
+
+                }else {
+                    Toast.makeText(NewUserActivity.this, "Authentication failed! Try Again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean isValidEmail(String email){
+        boolean isGoodEmail = (email!=null&& Patterns.EMAIL_ADDRESS.matcher(email).matches());
+
+        if (!isGoodEmail){
+            mEmailEditText.setError("Invalid email address");
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isValidPassword(String password,String confirmPassword){
+        if (password.length()<8){
+            mPasswordEditText.setError("The password must be at least 8 characters long");
+            return false;
+        }else if (!password.equals(confirmPassword)){
+            mPasswordEditText.setError("Passwords do not match");
+            return false;
+        }
+        return true;
+    }
 }
