d65cdafb488adb4c8141c5cc35706d9df7afbb73
lifereze <liferezearnold@gmail.com>
2020-08-09 22:53:52 +0300

    add a tweet class under the models folder

--- app/src/main/java/com/lifereze/tweeps/models/Tweet.java
+++ app/src/main/java/com/lifereze/tweeps/models/Tweet.java
@@ -1,4 +1,42 @@
 package com.lifereze.tweeps.models;
 
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Kenneth on 01/06/2017.
+ */
+
+@Parcel
 public class Tweet {
+    String mTweetText;
+    ArrayList<String> mHashTags;
+    String mExternalLink;
+    String mUser;
+
+    public Tweet(){}
+
+    public Tweet(String tweetText, ArrayList<String> hashTags,String externalLink,String user){
+        this.mTweetText = tweetText;
+        this.mHashTags = hashTags;
+        this.mExternalLink = externalLink;
+        this.mUser = user;
+    }
+
+    public String getTweetText(){
+        return mTweetText;
+    }
+
+    public ArrayList<String> getHashTags(){
+        return mHashTags;
+    }
+
+    public String getExternalLink(){
+        return mExternalLink;
+    }
+
+    public String getUser(){
+        return mUser;
+    }
 }
